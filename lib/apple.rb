d9e5097ef0624486c9069cd8ebfb9c8594c6ec13
codyss <cody.schwarz@gmail.com>
2015-10-15 23:25:56 -0400

    Done.

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,12 +1,29 @@
 def apple_picker_with_select(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just has apples in it
-  
+  fruit_list.select {|x| x == "apple"}
 end
 
 def apple_picker_with_collect(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that has just the apples, just like the 
   # challenge above
-  
+  result_of_collecting = fruit_list.collect {|x| x if x == "apple"}
+  result_of_collecting.compact
+end
+
+
+
+
+def apple_picker_with_collect2(fruit_list)
+  # fruit_list will look something like: ["apple", "banana", "apple"]
+  # you want to return an array that has just the apples, just like the 
+  # challenge above
+  flist = []
+  fruit_list.collect {|x| 
+    if x == "apple"
+      flist << x
+    end
+  }
+  flist
 end
